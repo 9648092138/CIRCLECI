9e7bd5f28da2c27b4c9e720d67543e0d3fd934fc
om prakash <op745891@gmail.com>
2020-10-30 11:26:44 +0530

    spring circle

---
+++ SpringbootCycleCi/src/main/java/com/op/ci/SpringbootCycleCiApplication.java
@@ -0,0 +1,13 @@
+package com.op.ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCycleCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCycleCiApplication.class, args);
+	}
+
+}
